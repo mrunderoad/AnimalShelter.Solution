c43316d2da97602306a8f1580b8fc2714797d586
Isaac Overstreet <isaacoverstreet3@gmail.com>
2022-04-01 09:46:25 -0700

    added length validations

---
+++ Shelter/Migrations/20220401164342_Validations.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shelter.Migrations
+{
+    public partial class Validations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Cats",
+                type: "varchar(20) CHARACTER SET utf8mb4",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Gender",
+                table: "Cats",
+                type: "varchar(10) CHARACTER SET utf8mb4",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Breed",
+                table: "Cats",
+                type: "varchar(50) CHARACTER SET utf8mb4",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Cats",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(20) CHARACTER SET utf8mb4",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Gender",
+                table: "Cats",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(10) CHARACTER SET utf8mb4",
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Breed",
+                table: "Cats",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50) CHARACTER SET utf8mb4",
+                oldMaxLength: 50);
+        }
+    }
+}
--- Shelter/Models/Cat.cs
+++ Shelter/Models/Cat.cs
@@ -1,11 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Shelter.Models
 {
   public class Cat
   {
     public int CatId { get; set; }
+    [Required]
+    [StringLength(20)]
     public string Name { get; set; }
+    [Required]
+    [StringLength(50)]
     public string Breed { get; set; }
+    [Required]
+    [Range(0, 30, ErrorMessage = "Age must be between 0 and 30.")]
     public int Age { get; set; }
+    [Required]
+    [StringLength(10)]
     public string Gender { get; set; }
   }
 }
