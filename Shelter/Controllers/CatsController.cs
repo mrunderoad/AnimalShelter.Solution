4217b2751da62e93b095e23d6155a4b74185b516
Isaac Overstreet <isaacoverstreet3@gmail.com>
2022-04-01 08:43:42 -0700

    PUT request for cats working

--- Shelter/Controllers/CatsController.cs
+++ Shelter/Controllers/CatsController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Shelter.Models;
+using System.Linq;
 
 namespace Shelter.Controllers
 {
@@ -30,6 +31,53 @@ namespace Shelter.Controllers
       await _db.SaveChangesAsync();
 
       return CreatedAtAction("Post", new { id = cat.CatId }, cat);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Cat>> GetCat(int id)
+    {
+      var cat = await _db.Cats.FindAsync(id);
+
+      if (cat == null)
+      {
+        return NotFound();
+      }
+
+      return cat;
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Cat cat)
+    {
+      if (id != cat.CatId)
+      {
+        return BadRequest();
+      }
+
+      _db.Entry(cat).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!CatExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    private bool CatExists(int id)
+    {
+      return _db.Cats.Any(e => e.CatId == id);
     }
   }
 }
