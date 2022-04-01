90ccda62465f0aec05affb6dc642b39b24a81905
Isaac Overstreet <isaacoverstreet3@gmail.com>
2022-04-01 11:27:11 -0700

    versioning for dogs complete

--- Shelter/Controllers/DogsController.cs
+++ Shelter/Controllers/DogsController.cs
@@ -119,4 +119,117 @@ namespace Shelter.Controllers
       return NoContent();
     }
   }
+
+  [ApiVersion("2.0")]
+  [Route("api/{v:apiVersion}/dogs")]
+  [ApiController]
+  public class DogsV2Controller : ControllerBase
+  {
+    private readonly ShelterContext _db;
+
+    public DogsV2Controller(ShelterContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Dog>>> Get(string name, string breed, int minAge, string gender)
+    {
+      var query = _db.Dogs.AsQueryable();
+
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+
+      if (breed != null)
+      {
+        query = query.Where(entry => entry.Breed == breed);
+      }
+
+      if (minAge > 0)
+      {
+        query = query.Where(entry => entry.Age >= minAge);
+      }
+
+      if (gender != null)
+      {
+        query = query.Where(entry => entry.Gender == gender);
+      }
+
+      return await query.ToListAsync();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Dog>> Post(Dog dog)
+    {
+      _db.Dogs.Add(dog);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction("Post", new { id = dog.DogId }, dog);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Dog>> GetDog(int id)
+    {
+      var dog = await _db.Dogs.FindAsync(id);
+
+      if (dog == null)
+      {
+        return NotFound();
+      }
+
+      return dog;
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Dog dog)
+    {
+      if (id != dog.DogId)
+      {
+        return BadRequest();
+      }
+
+      _db.Entry(dog).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!DogExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    private bool DogExists(int id)
+    {
+      return _db.Dogs.Any(e => e.DogId == id);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteDog(int id)
+    {
+      var dog = await _db.Dogs.FindAsync(id);
+      
+      if (dog == null)
+      {
+        return NotFound();
+      }
+
+      _db.Dogs.Remove(dog);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
+  }
 }
